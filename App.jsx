726df6507b75e571f82c92001480a6825c14e983
Shravanya178 <2023.shravanya.andhale@ves.ac.in>
2025-03-02 02:42:42 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Schedule from "./schedule";
+
+function App() {
+  return (
+    <div>
+      <h1>Medical Reminder App</h1>
+      <MedicalSchedule />
+    </div>
+  );
+}
+
+export default App;
