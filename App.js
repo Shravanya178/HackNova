15b10f008549d72691023705420f2ed7daa083f3
Shravanya178 <2023.shravanya.andhale@ves.ac.in>
2025-03-02 11:55:31 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, useLocation, useNavigate } from 'react-router-dom';
+import React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
 import { MapPin, Settings } from "lucide-react";
-import DashboardPage from './components/DashboardPage';
-import PharmacyPage from './components/PharmacyPage';
-import Reminder from './components/Reminder';
-import SchedulePage from './components/SchedulePage';
-import SOSPage from './components/SOSPage';
-import SettingsPage from './components/SettingsPage';
-import LogsPage from './components/LogsPage';
-import HelpPage from './components/HelpPage';
+import DashboardPage from "./components/DashboardPage";
+import PharmacyPage from "./components/PharmacyPage";
+import Reminder from "./components/Reminder";
+import SchedulePage from "./components/SchedulePage";
+import SOSPage from "./components/SOSPage";
+import SettingsPage from "./components/SettingsPage";
+import LogsPage from "./components/LogsPage";
+import HelpPage from "./components/HelpPage";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
@@ -16,13 +23,14 @@ function App() {
       <Routes>
         <Route path="/" element={<DashboardPage />} />
         <Route path="/pharmacy" element={<PharmacyPage />} />
-        <Route path="/reminder" element={<Reminder/>} />
-        <Route path="/schedule" element={<SchedulePage/>} />
-        <Route path="/sos" element={<SOSPage/>} />
-        <Route path="/Help" element={<HelpPage/>} />
-        <Route path="/settings" element={<SettingsPage/>} />
-        <Route path="/profile" element={<LogsPage/>} />
-        <Route path="/privacy" element={<SOSPage/>} />
+        <Route path="/reminder" element={<Reminder />} />
+        <Route path="/schedule" element={<SchedulePage />} />
+        <Route path="/sos" element={<SOSPage />} />
+        <Route path="/Help" element={<HelpPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={<LogsPage />} />
+        <Route path="/privacy" element={<SOSPage />} />
+        <Route path="/NavBar" element={<NavBar />} />
       </Routes>
     </BrowserRouter>
   );
