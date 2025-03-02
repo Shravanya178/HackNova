dd6d56a07f01519d32e1dcb65ae6b47d4f438a7e
Shravanya178 <2023.shravanya.andhale@ves.ac.in>
2025-03-02 12:09:34 +0530

    Update DashboardPage.jsx

--- components/DashboardPage.jsx
+++ components/DashboardPage.jsx
@@ -145,7 +145,7 @@ const DashboardPage = () => {
           <div className="relative flex items-center w-full px-8">
             <button
               className="absolute left-1/2 transform -translate-x-1/2 border-2 border-black rounded-full w-12 h-12 flex items-center justify-center"
-              onClick={() => navigate("/add-medication")}
+              onClick={() => navigate("/Reminder")}
             >
               <Plus size={24} />
             </button>
